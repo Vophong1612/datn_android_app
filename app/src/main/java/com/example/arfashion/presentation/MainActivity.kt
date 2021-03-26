3a6e8a8c7f416bcd536d9770b455a2acf2c0ccf2
phucarus112 <huynhphuc211.thcs@gmail.com>
2021-03-26 08:47:42 +0700

    Issue #3 - [Feature]Forgot Password, User Info - design

--- app/src/main/java/com/example/arfashion/presentation/MainActivity.kt
+++ app/src/main/java/com/example/arfashion/presentation/MainActivity.kt
@@ -2,11 +2,26 @@ package com.example.arfashion.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.example.arfashion.R
 
 class MainActivity : AppCompatActivity() {
+    val arr = arrayOf<String>("Tp Hồ Chí Minh, TP Hà Nội, Đà Nẵng")
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_choose_address)
+//        setUpSpinner()
     }
+
+
+//    fun setUpSpinner(){
+//        langAdapter?.setDropDownViewResource(R.layout.layout_simple_spinner_dropdown)
+//        provinceSpinner.adapter = langAdapter
+//        districtSpinner.adapter = langAdapter
+//        wardSpinner.adapter = langAdapter
+//    }
+
 }
