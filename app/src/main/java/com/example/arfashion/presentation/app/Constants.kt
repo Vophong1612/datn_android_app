c07b47551c9b072fec3c6b00bddc0d48facf752a
vophong1612 <vophong1612@gmail.com>
2021-06-22 12:30:09 +0700

    Issue #39 - [BugFix] Fix Crash App when register

--- app/src/main/java/com/example/arfashion/presentation/app/Constants.kt
+++ app/src/main/java/com/example/arfashion/presentation/app/Constants.kt
@@ -1,5 +1,6 @@
 package com.example.arfashion.presentation.app
 
 object Constants {
-    const val API_URL = "http://3.139.38.197:3000"
+//    const val API_URL = "http://3.139.38.197:3000"
+    const val API_URL = "https://ar-fashion-api.herokuapp.com"
 }
